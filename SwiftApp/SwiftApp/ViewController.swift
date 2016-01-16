759e4770639ae593a18adf246ea5d47a50ca2347
longlong <15966960596@163.com>
2016-01-16 17:19:38 +0800

    创建项目

---
+++ SwiftApp/SwiftApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by Wangjianlong on 16/1/16.
+//  Copyright © 2016年 JL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
